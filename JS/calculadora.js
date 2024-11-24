fb9c442b4b198f7fce783de169e4d3b50a47e0cd
Alexis <ferguanoluisa99@hotmail.com>
2024-11-24 01:36:20 +0000

    Tarea 5

--- JS/calculadora.js
+++ JS/calculadora.js
@@ -1,40 +1,93 @@
-var num1=null;
-var num2=null;
+var num1 = null;
+var num2 = null;
 var operacion = '';
-function setearValor(valor){
-    let valorActual=document.getElementById('id-display').innerText;
-    console.log(valorActual)
+var operando = false;
+
+function setearValor(valor) {
     document.getElementById('id-display').innerText = valor;
 }
-function setearValorConcatenado(valor){
-    let valorActual=document.getElementById('id-display').innerText;
-    console.log(valorActual)
-    document.getElementById('id-display').innerText = valorActual+valor;
+function setearValorConcatenado(valor) {
+    let valorActual = document.getElementById('id-display').innerText;
+    document.getElementById('id-display').innerText = valorActual + valor;
 }
 
-function recibirValor(valor){
-    console.log(valor);
-    if(num1===null){
-        num1=valor
-    }else{
-        num2=valor;
+function recibirValor(valor) {
+
+    if (!operando) {
+        if (num1 === null) {
+            num1 = valor.toString();
+            setearValor(num1);
+        } else {
+            num1 += valor.toString();
+            setearValor(num1);
+        }
+    } else {
+        if (num2 === null) {
+            num2 = valor.toString();
+            setearValor(num2);
+        } else {
+            num2 += valor.toString();
+            setearValor(num2);
+        }
     }
 }
 
-function evaluarOperacion(op){
-    operacion=op
+function evaluarOperacion(op) {
+    if (num1 !== null) {
+        operacion = op;
+        operando = true;
+        setearValor(operacion);
+    }
+
 }
 
-function igual(){
+function igual() {
     let valorFinal;
-    if(operacion ==='+' ){
-        valorFinal= num1+num2;
-    }else if(operacion==='-'){
-        valorFinal=num1-num2;
-    }else if (operacion==='X'){
-        valorFinal=num1*num2
-    }else if(operacion==='%'){
-        valorFinal= num1/num2
+    if (num1 !== null && num2 !== null) {
+        let n1 = parseFloat(num1);
+        let n2 = parseFloat(num2);
+
+        if (operacion === '+') {
+            valorFinal = n1 + n2;
+        } else if (operacion === '-') {
+            valorFinal = n1 - n2;
+        } else if (operacion === 'X') {
+            valorFinal = n1 * n2;
+        } else if (operacion === '%') {
+            valorFinal = n1 / n2;
+            valorFinal.toFixed(5);
+        }
+        setearValor(valorFinal);
     }
-    setearValor(valorFinal);
+
+}
+
+function reset() {
+    num1 = null;
+    num2 = null;
+    operacion = '';
+    operando = false;
+    setearValor('0')
+
+}
+
+function borrar() {
+
+    let display = document.getElementById('id-display').innerText;
+
+    if (!operando) {
+        if (num1 !== null && num1.length > 0) {
+            num1 = num1.slice(0, -1);
+            setearValor(num1 || '0');
+        }
+    } else {
+        if (num2 !== null && num2.length > 0) {
+            num2 = num2.slice(0, -1);
+            setearValor(num2 || operacion);
+        } else if (num2 === null && operacion.length > 0) {
+            operacion = '';
+            operando = false;
+            setearValor(num1 || '0');
+        }
+    }
 }
